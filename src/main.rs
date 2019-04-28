44a9ac00a0675bda1a1b96b1888c4b23a352ec2b
Peter Coleman <peter.coleman143@gmail.com>
2019-04-29 07:21:51 +1000

    added vga buffer module

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,14 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
+use core::panic::PanicInfo;
+use vga_buffer::print_something;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    print_something();
 
     loop {}
 }
