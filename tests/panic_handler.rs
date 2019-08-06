98c9183701c6dbab4d5a1547b47a54564155b53e
Peter Coleman <peter.coleman143@gmail.com>
2019-08-07 07:04:32 +1000

    Finished panic handler test

--- tests/panic_handler.rs
+++ tests/panic_handler.rs
@@ -1,11 +1,14 @@
 #![no_std]
 #![no_main]
+#![feature(panic_info_message)]
 
 use core::panic::PanicInfo;
+use core::fmt;
+use core::fmt::Write;
 use rust_os::{serial_print, serial_println, QemuExitCode, exit_qemu};
 
 const MESSAGE: &str = "Example panic message from panic_handler test";
-const PANIC_LINE: u32 = 14;
+const PANIC_LINE: u32 = 16;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -15,8 +18,57 @@ pub extern "C" fn _start() -> ! {
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
+	check_message(info);
+	check_location(info);
+
 	serial_println!("[ok]");
 	exit_qemu(QemuExitCode::Success);
 	loop {}
 }
+
+fn fail(error: &str) -> ! {
+	serial_println!("[failed]");
+	serial_println!("{}", error);
+	exit_qemu(QemuExitCode::Failed);
+	loop {}
+}
+
+fn check_location(info: &PanicInfo) {
+	let location = info.location().unwrap_or_else(||fail("no location"));
+	if location.file() != file!() {
+		fail("file name wrong");
+	}
+
+	if location.line() != PANIC_LINE {
+		fail("file line wrong");
+	}
+}
+
+fn check_message(info: &PanicInfo) {
+	let message = info.message().unwrap_or_else(|| fail("no message"));
+	let mut compare_message = CompareMessage { expected: MESSAGE };
+	write!(&mut compare_message, "{}", message)
+		.unwrap_or_else(|_| fail("write failed"));
+	if !compare_message.expected.is_empty() {
+		fail("message shorter than expected message");
+	}
+}
+
+struct CompareMessage {
+	expected: &'static str,
+}
+
+
+impl fmt::Write for CompareMessage {
+	/// Ensures the given string is expected
+	fn write_str(&mut self, s: &str) -> fmt::Result {
+		if self.expected.starts_with(s) {
+			self.expected = &self.expected[s.len()..];
+		} else {
+			fail("message not equal to expected message");
+		}
+		Ok(())
+	}
+}
+
 
